2ad523382326826f39bcea3d9299ea292370ef8c
KKramer03 <kevinjkramer168@gmail.com>
2025-10-09 13:04:53 -0400

    CORS test

--- apps/api/src/cors.config.ts
+++ apps/api/src/cors.config.ts
@@ -1,6 +1,6 @@
 const whitelist: string[] = [
   'http://localhost:3001',
-  'https://cisc474-lms-project.kevinjkramer168.workers.dev/', // Cloudflare deployment URL
+  'https://cisc474-lms-project.kevinjkramer168.workers.dev', // Cloudflare deployment URL
 ];
 
 export const cors = {
