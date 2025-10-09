672486e482662e8b71267d7c561eb0efdcae6ce3
KKramer03 <kevinjkramer168@gmail.com>
2025-10-09 12:51:44 -0400

    changed CORS whitelist to use cloudflare deployment url

--- apps/api/src/cors.config.ts
+++ apps/api/src/cors.config.ts
@@ -1,7 +1,6 @@
 const whitelist: string[] = [
   'http://localhost:3001',
-  'https://f25-cisc474-individual-pj71t1c02-kevins-projects-3fcdc5db.vercel.app/', // Personal Link
-  'https://f25-cisc474-individual-web-git-main-kevins-projects-3fcdc5db.vercel.app/', // Shared Link
+  'https://cisc474-lms-project.kevinjkramer168.workers.dev/', // Cloudflare deployment URL
 ];
 
 export const cors = {
